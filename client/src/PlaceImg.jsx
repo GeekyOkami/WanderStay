06abea26f7c2d4cb88e5aa72cf3f0622288f1c2c
GeekyOkami <97523703+GeekyOkami@users.noreply.github.com>
2024-08-14 15:38:20 +0530

    Update PlaceImg.jsx

--- client/src/PlaceImg.jsx
+++ client/src/PlaceImg.jsx
@@ -8,7 +8,7 @@ function PlaceImg({ place, index = 0, className }) {
         className = 'object-cover';
     }
     return (
-        <img className={className} src={'http://localhost:3000/uploads/' + place.photos[index]} alt="" />
+        <img className={className} src={'https://wanderstay-backend.onrender.com/uploads/' + place.photos[index]} alt="" />
     )
 }
 

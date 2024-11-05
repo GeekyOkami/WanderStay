d965c232b9c93c4f162483d2756dc62a9a523c9c
GeekyOkami <97523703+GeekyOkami@users.noreply.github.com>
2024-11-05 20:12:45 +0530

    Update UserContext.jsx

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -10,7 +10,7 @@ export function UserContextProvider({children}){
   
   useEffect(() => {
       if (!user) {
-          axios.get('/profile')
+          axios.get('/profile',{ withCredentials: true })
               .then(({data}) => {
                   setUser(data);
                   setReady(true);
